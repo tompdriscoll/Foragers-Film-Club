acdfb4d1847140c3ee91f689ddd71a35cf799d61
Tom Driscoll <tdriscoll1@yahoo.com>
2020-08-18 13:45:13 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/foragers.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        include: /images/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
+
+
