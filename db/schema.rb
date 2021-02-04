10cbcf31ed92bd8206a1ed1aaec8ebd9c0a4cce3
Tom Driscoll <tdriscoll1@yahoo.com>
2021-02-04 11:42:27 -0500

    migration to change type column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_153308) do
+ActiveRecord::Schema.define(version: 2021_02_04_164035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "event_type"
     t.integer "hostId"
     t.datetime "time"
     t.datetime "created_at", null: false
